a983d29f727dd611f40fa2598284374ec5faa04e
Julia Linhart <julialinhart@MacBook-of-Julia-Linhart-2.local>
2022-03-18 14:40:37 +0100

    different target distributions: gmm, exp

--- plot.py
+++ plot.py
@@ -7,15 +7,20 @@ import torch.distributions as D
 
 def plot_pdf_1D(target_dist, x_samples, x_i, x_f, flow=None):
     eval_x = torch.linspace(x_i, x_f).reshape(-1,1)
-    
-    p_x_true = torch.exp(target_dist.log_prob(eval_x))
-    if flow is not None:
-        p_x_learned = torch.exp(flow.log_prob(eval_x))
 
     fig = plt.figure(figsize=(6, 2))
     plt.plot(x_samples, np.zeros_like(x_samples), 'bx', alpha=0.5, markerfacecolor='none', markersize=6)
-    plt.plot(eval_x.numpy(), p_x_true.detach().numpy(),'--', color='blue')
+    
+    try:
+        p_x_true = torch.exp(target_dist.log_prob(eval_x))
+        plt.plot(eval_x.numpy(), p_x_true.detach().numpy(),'--', color='blue')
+    except ValueError:  # in case of exponential distribution 
+        eval_x_pos = torch.linspace(0.01, x_f).reshape(-1,1)
+        p_x_true = torch.exp(target_dist.log_prob(eval_x_pos))
+        plt.plot(eval_x_pos.numpy(), p_x_true.detach().numpy(),'--', color='blue')
+
     if flow is not None:
+        p_x_learned = torch.exp(flow.log_prob(eval_x))
         plt.plot(eval_x.numpy(), p_x_learned.detach().numpy(), color='orange')
 
     plt.legend(["Samples", "True", "Learned"], loc="upper right")
@@ -26,8 +31,15 @@ def plot_pdf_1D(target_dist, x_samples, x_i, x_f, flow=None):
 
 def plot_cdf_1D(target_dist, flow, x_i, x_f, base_dist=D.Normal(0,1)):
     x_eval = torch.linspace(x_i,x_f).reshape(-1,1)
-    cdf_estimate = cdf_flow(x_eval, flow, base_dist)
-    plt.plot(x_eval,target_dist.cdf(x_eval), color='blue')
+    
+    try:
+        cdf_estimate = cdf_flow(x_eval, flow, base_dist)
+        plt.plot(x_eval,target_dist.cdf(x_eval), color='blue')
+    except ValueError:  # in case of exponential distribution 
+        x_eval_pos = torch.linspace(0.01, x_f).reshape(-1,1)
+        cdf_estimate = cdf_flow(x_eval_pos, flow, base_dist)
+        plt.plot(x_eval,target_dist.cdf(x_eval_pos), color='blue')
+
     plt.plot(x_eval, cdf_estimate.detach().numpy(), color='orange')
     plt.legend(["True", "Learned"], loc="upper left")
     plt.show()

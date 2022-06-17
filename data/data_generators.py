045fd93b5b852f9e8fe29d498b3ec6e987f3b094
Julia Linhart <julialinhart@MacBook-of-Julia-Linhart-2.local>
2022-06-17 19:57:46 +0200

    SBI toy example

--- data/data_generators.py
+++ data/data_generators.py
@@ -14,16 +14,14 @@ class ConditionalGaussian1d:
     def __init__(self, mu=[0, 0], sigma=[1, 1], rho=0.8) -> None:
         # Distribution of X: 2d gaussian
         self.mean = torch.FloatTensor(mu)
-        self.covariance = torch.FloatTensor(
+        self.cov = torch.FloatTensor(
             [
                 [sigma[0] ** 2, rho * sigma[0] * sigma[1]],
                 [rho * sigma[0] * sigma[1], sigma[1] ** 2],
             ]
         )
 
-        self.x_dist = MultivariateNormal(
-            loc=self.mean, covariance_matrix=self.covariance
-        )
+        self.x_dist = MultivariateNormal(loc=self.mean, covariance_matrix=self.cov)
 
     def sample_x(self, n):
         return self.x_dist.sample((n,))
@@ -46,7 +44,7 @@ class ConditionalGaussian1d:
         # Joint PIT-values [PIT(Theta_i, X_i, f_{Theta|X})]
         if local:
             pit_values = norm.cdf(
-                samples_theta, loc=samples_x[:,0] + samples_x[:,1], scale=1
+                samples_theta, loc=samples_x[:, 0] + samples_x[:, 1], scale=1
             )
         else:
             pit_values = np.array(
@@ -56,3 +54,62 @@ class ConditionalGaussian1d:
                 ]
             )
         return pit_values
+
+
+class SBIGaussian2d:
+    def __init__(self, prior, x_correlation_factor=0.8) -> None:
+        """2d Gaussian.
+        Inference of mean under given prior.
+        """
+        self.prior = prior
+        self.x_correlation_factor = x_correlation_factor
+
+    def simulator(self, theta):
+        # Distribution parameters
+        rho = self.x_correlation_factor
+        cov = torch.FloatTensor([[1, rho], [rho, 1]])
+        # Sample X
+        samples_x = MultivariateNormal(loc=theta, covariance_matrix=cov).sample((1,))[0]
+        return samples_x
+
+    def get_joint_data(self, n):
+        samples_prior = self.prior.sample((n,))
+        samples_x = self.simulator(samples_prior)
+        return samples_prior, samples_x
+
+    def true_posterior_pdf(self, x_obs):
+        def true_posterior_prob(samples):
+            log_p_prior = self.prior.log_prob(samples)
+            rho = self.x_correlation_factor
+            cov = torch.FloatTensor([[1, rho], [rho, 1]])
+            if x_obs.ndim > 1:
+                log_p_x = MultivariateNormal(
+                    loc=samples, covariance_matrix=cov
+                ).log_prob(torch.mean(x_obs, axis=0))
+            else:
+                log_p_x = MultivariateNormal(
+                    loc=samples, covariance_matrix=cov
+                ).log_prob(x_obs)
+            log_p = log_p_prior + log_p_x
+            return get_proba(log_p)
+        return true_posterior_prob
+    
+    def true_posterior(self, x_obs):
+        rho = self.x_correlation_factor
+        cov = torch.FloatTensor([[1, rho], [rho, 1]])
+        return MultivariateNormal(loc=x_obs, covariance_matrix=cov)
+
+
+def get_proba(log_p):
+    """Compute probability from log_prob in a safe way.
+
+    Parameters
+    ----------
+    log_p: ndarray, shape (n_samples*n_samples,)
+        Values of log_p for each point (a,b) of the samples (n_samples, n_samples).
+    """
+    if isinstance(log_p, np.ndarray):
+        log_p = torch.tensor(log_p)
+    log_p = log_p.to(dtype=torch.float64)
+    log_p -= torch.logsumexp(log_p, dim=-1)
+    return torch.exp(log_p)
--- plot.py
+++ plot.py
@@ -6,116 +6,183 @@ import numpy as np
 import torch.distributions as D
 import seaborn as sns
 
+
 def plot_pdf_1D(x_samples, x_i, x_f, target_dist=None, flows=None, context=None):
-    """`flows` must be a dict with the plot-label as key_name and 3 elements: (flow, context, plot-color). """
-    eval_x = torch.linspace(x_i, x_f).reshape(-1,1)
+    """`flows` must be a dict with the plot-label as key_name and 3 elements: (flow, context, plot-color)."""
+    eval_x = torch.linspace(x_i, x_f).reshape(-1, 1)
     if context is not None:
-        context = context.repeat(eval_x.size(0),1)
+        context = context.repeat(eval_x.size(0), 1)
 
     fig = plt.figure(figsize=(6, 2))
-    plt.plot(x_samples, np.zeros_like(x_samples), 'bx', alpha=0.5, markerfacecolor='none', markersize=6)
-    labels=['Samples']
-    
+    plt.plot(
+        x_samples,
+        np.zeros_like(x_samples),
+        "bx",
+        alpha=0.5,
+        markerfacecolor="none",
+        markersize=6,
+    )
+    labels = ["Samples"]
+
     if target_dist is not None:
-        labels+=['True']
+        labels += ["True"]
         try:
             p_x_true = torch.exp(target_dist.log_prob(eval_x, context=context))
-            plt.plot(eval_x.numpy(), p_x_true.detach().numpy(),'--', color='blue')
-        except ValueError:  # in case of exponential distribution 
-            eval_x_pos = torch.linspace(0.01, x_f).reshape(-1,1)
+            plt.plot(eval_x.numpy(), p_x_true.detach().numpy(), "--", color="blue")
+        except ValueError:  # in case of exponential distribution
+            eval_x_pos = torch.linspace(0.01, x_f).reshape(-1, 1)
             p_x_true = torch.exp(target_dist.log_prob(eval_x_pos, context=context))
-            plt.plot(eval_x_pos.numpy(), p_x_true.detach().numpy(),'--', color='blue')
+            plt.plot(eval_x_pos.numpy(), p_x_true.detach().numpy(), "--", color="blue")
 
     if flows is not None:
         for flow, ct, col in list(flows.values()):
             if ct is not None:
-                ct = ct.repeat(eval_x.size(0),1)
+                ct = ct.repeat(eval_x.size(0), 1)
             p_x_learned = torch.exp(flow.log_prob(eval_x, context=ct))
             plt.plot(eval_x.numpy(), p_x_learned.detach().numpy(), color=col)
         labels += list(flows.keys())
-    
+
     plt.legend(labels)
 
-    _ = plt.xlim([x_i, x_f]); _ = plt.ylim([-0.12, 1.2])
+    _ = plt.xlim([x_i, x_f])
+    _ = plt.ylim([-0.12, 1.2])
 
     plt.show()
 
-def plot_cdf_1D(target_dist, flow, x_i, x_f, base_dist=D.Normal(0,1)):
-    x_eval = torch.linspace(x_i,x_f).reshape(-1,1)
-    
+
+def plot_cdf_1D(target_dist, flow, x_i, x_f, base_dist=D.Normal(0, 1)):
+    x_eval = torch.linspace(x_i, x_f).reshape(-1, 1)
+
     try:
         cdf_estimate = cdf_flow(x_eval, flow, base_dist)
-        plt.plot(x_eval,target_dist.cdf(x_eval), color='blue')
-    except ValueError:  # in case of exponential distribution 
-        x_eval_pos = torch.linspace(0.01, x_f).reshape(-1,1)
+        plt.plot(x_eval, target_dist.cdf(x_eval), color="blue")
+    except ValueError:  # in case of exponential distribution
+        x_eval_pos = torch.linspace(0.01, x_f).reshape(-1, 1)
         cdf_estimate = cdf_flow(x_eval_pos, flow, base_dist)
-        plt.plot(x_eval,target_dist.cdf(x_eval_pos), color='blue')
+        plt.plot(x_eval, target_dist.cdf(x_eval_pos), color="blue")
 
-    plt.plot(x_eval, cdf_estimate.detach().numpy(), color='orange')
+    plt.plot(x_eval, cdf_estimate.detach().numpy(), color="orange")
     plt.legend(["True", "Learned"], loc="upper left")
     plt.show()
 
-def PP_plot_1D(target_dist, x_samples, flow, base_dist=D.Normal(0,1)):
-    alphas = np.linspace(0,1)
+
+def PP_plot_1D(target_dist, x_samples, flow, base_dist=D.Normal(0, 1)):
+    alphas = np.linspace(0, 1)
     cdf = lambda x: cdf_flow(x, flow, base_dist)
     z_true = PP_vals(target_dist.cdf, x_samples, alphas)
     z_estimate = PP_vals(cdf, x_samples, alphas)
-    plt.plot(alphas, z_true, color='blue')
-    plt.plot(alphas, z_estimate, color='orange')
+    plt.plot(alphas, z_true, color="blue")
+    plt.plot(alphas, z_estimate, color="orange")
     plt.legend(["True", "Learned"], loc="upper left")
     plt.show()
-
 
 
 def PP_vals(cdf, x_samples, alphas):
     """Return estimated vs true quantiles between pdist and samples.
     inputs:
-    - cdf: cdf of the estimate (flow) 
+    - cdf: cdf of the estimate (flow)
     - x_samples: torch.tensor of samples of the target distribution
-    - alphas: array of values to evaluate the PP-vals 
+    - alphas: array of values to evaluate the PP-vals
     """
     F = cdf(x_samples).detach().numpy()
-    z = [np.mean(F < alpha)  for alpha in alphas]
+    z = [np.mean(F < alpha) for alpha in alphas]
     return z
 
 
-    
-def cdf_flow(x, flow, base_dist=D.Normal(0,1)):
-    """ Return the cdf of the flow evaluated in x
+def cdf_flow(x, flow, base_dist=D.Normal(0, 1)):
+    """Return the cdf of the flow evaluated in x
     input:
     - x: torch.tensor
-    - flow: nflows.Flow 
+    - flow: nflows.Flow
     - base_dist: torch.distributions object
-        """
+    """
     return base_dist.cdf(flow._transform(x)[0])
 
 
 from matplotlib.lines import Line2D
 
 ### 2D ###
-# 2D distributions: Evaluate the learnt transformation by plotting the learnt 2D pdf-contours 
+# 2D distributions: Evaluate the learnt transformation by plotting the learnt 2D pdf-contours
 # against the true pdf-contours and the training samples (drawn from the true distribution)
-def plot_2d_pdf_contours(target_dist, x_samples, flow, title=None, n=500, gaussian=False):
+def plot_2d_pdf_contours(
+    target_dist, flow, x_samples=None, title=None, n=500, gaussian=False
+):
     x_true = target_dist.sample((n,))  # Sample from groundtruth
     x_learned = flow.sample(n).detach().numpy()  # Sample from learned
-
-    plt.scatter(x=x_samples[:,0], y=x_samples[:,1], color='blue', label="Samples") # Plot training samples
-    sns.kdeplot(x=x_true[:,0], y=x_true[:,1], color='blue')  # Plot true distribution
-    sns.kdeplot(x=x_learned[:,0], y=x_learned[:,1], color='orange')  # Plot learned distribution
+    if x_samples is not None:
+        plt.scatter(
+            x=x_samples[:, 0], y=x_samples[:, 1], color="blue", label="Samples"
+        )  # Plot training samples
+    sns.kdeplot(x=x_true[:, 0], y=x_true[:, 1], color="blue")  # Plot true distribution
+    sns.kdeplot(
+        x=x_learned[:, 0], y=x_learned[:, 1], color="orange"
+    )  # Plot learned distribution
 
     handles, labels = plt.gca().get_legend_handles_labels()
 
-    handles.extend([
-        Line2D([0], [0], color='blue', label='True'), 
-        Line2D([0], [0], color='orange', label='Learned')
-        ])
-    
+    handles.extend(
+        [
+            Line2D([0], [0], color="blue", label="True"),
+            Line2D([0], [0], color="orange", label="Learned"),
+        ]
+    )
+
     plt.legend(handles=handles, loc="upper right")
 
     if gaussian:
         means_learned = np.mean(x_learned, axis=0)  # Learned mean
-        plt.scatter(x=target_dist.mean[0], y=target_dist.mean[1], color='cyan')
-        plt.scatter(x=means_learned[0], y=means_learned[1], color='magenta')
-    
+        plt.scatter(x=target_dist.mean[0], y=target_dist.mean[1], color="cyan")
+        plt.scatter(x=means_learned[0], y=means_learned[1], color="magenta")
+
     plt.title(title)
+    plt.show()
+
+
+def get_grid(low, high, n_samples=1000):
+    """Return grid for evaluation.
+
+    Parameters
+    ----------
+    n_samples : int
+        Number of samples, number of points in linspace.
+
+    Returns
+    -------
+    samples : torch.Tensor, shape (n_samples*n_samples, 2)
+    XX, YY : torch.Tensor, shape (n_samples, n_samples)
+    """
+    t = torch.linspace(low, high, n_samples)
+    XX, YY = torch.meshgrid(t, t)
+    samples = torch.cat([XX.reshape(-1, 1), YY.reshape(-1, 1)], dim=1)
+
+    return samples, XX, YY
+
+
+def plot_2d_pdf_on_grid(pdf, low, high, ground_truth=None):
+    """Plot analytic and learned posterior on grid returned by the function get_grid() above.
+
+    Parameters
+    ----------
+    pdf : pdf function
+        Computes the probability of a given point
+    low, high: lower and upper border of the 2d-grid
+
+    Returns
+    -------
+    fig, ax : plt.figure and axes
+    """
+    samples, XX, YY = get_grid(low=low, high=high)
+    probas_on_grid = pdf(samples)
+    # Plot true distribution
+    plt.contour(
+        XX.numpy(),
+        YY.numpy(),
+        probas_on_grid.reshape(1000, 1000),
+        cmap="Blues",
+        zorder=0,
+    )
+    if ground_truth is not None:
+        plt.scatter(
+            ground_truth[0], ground_truth[1], marker="*", color="red", s=100, zorder=1
+        )
     plt.show()

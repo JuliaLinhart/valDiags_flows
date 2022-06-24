4804f46e7e3277cb3e37a4c65fd1759d5277e60f
Julia Linhart <julialinhart@MacBook-of-Julia-Linhart-2.local>
2022-06-24 11:48:46 +0200

    waldo stat func

--- diagnostics/confidence_regions.py
+++ diagnostics/confidence_regions.py
@@ -38,34 +38,38 @@ def hpd_region(
 
 
 def waldo_confidence_region(
-    posterior_samples_obs, predicted_cutoffs_nn, param_grid, grid_sample_size
+    posterior_samples, critical_values, param_grid, grid_sample_size
 ):
     """Calibrated Confidence Set for learned flow posterior using Waldo statistic.
     Code adapted from WALDO git-repo."""
 
     # compute posterior mean and variance
-    posterior_mean_obs = torch.mean(posterior_samples_obs, dim=0)
-    posterior_var_obs = torch.cov(torch.transpose(posterior_samples_obs, 0, 1))
+    posterior_mean = torch.mean(posterior_samples, dim=0)
+    posterior_var = torch.cov(torch.transpose(posterior_samples, 0, 1))
     confidence_set_i = []
     for j in range(grid_sample_size):
         # compute waldo stats for every theta in the grid
-        obs_statistics = torch.matmul(
-            torch.matmul(
-                torch.t(
-                    torch.subtract(
-                        posterior_mean_obs.reshape(-1, 1),
-                        torch.from_numpy(param_grid[j, :]).float().reshape(-1, 1),
-                    )
-                ),
-                torch.linalg.inv(posterior_var_obs),
-            ),
-            torch.subtract(
-                posterior_mean_obs.reshape(-1, 1),
-                torch.from_numpy(param_grid[j, :]).float().reshape(-1, 1),
-            ),
-        ).item()
+        obs_statistics = waldo_stats(
+            posterior_mean,
+            posterior_var,
+            param=torch.from_numpy(param_grid[j, :]).float(),
+        )
         # compare to critical value
-        if obs_statistics <= predicted_cutoffs_nn[j]:
+        if obs_statistics <= critical_values[j]:
             confidence_set_i.append(param_grid[j, :].reshape(1, 2))
     confidence_set = np.vstack(confidence_set_i)
     return confidence_set
+
+
+def waldo_stats(posterior_mean, posterior_var, param):
+    stats = torch.matmul(
+        torch.matmul(
+            torch.t(
+                torch.subtract(posterior_mean.reshape(-1, 1), param.reshape(-1, 1))
+            ),
+            torch.linalg.inv(posterior_var),
+        ),
+        torch.subtract(posterior_mean.reshape(-1, 1), param.reshape(-1, 1)),
+    ).item()
+    return stats
+

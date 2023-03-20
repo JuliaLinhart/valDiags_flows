5f6d92f85410d5267d4559d80f32eeb165681f67
JuliaLinhart <julia_linhart@yahoo.com>
2023-03-20 18:07:35 +0100

    empirical powere exp

--- eval_clf_test_stats.py
+++ eval_clf_test_stats.py
@@ -16,7 +16,7 @@ from tqdm import tqdm
 DIM = 5
 N_SAMPLES = 100
 
-s = np.sqrt(0.05)
+mu = np.sqrt(0.05)
 
 metrics = ["accuracy", "div", "mse"]
 n_alpha = 20
@@ -25,13 +25,13 @@ n_runs = 300
 # ESTIMATED LDA
 power = dict(zip(metrics, [[] for _ in range(len(metrics))]))
 for i, alpha in enumerate(np.linspace(0, 1, n_alpha)):
-    print(f"alpha {i+1}/{n_alpha} = {alpha}")
     print(f"Computing empirical error as the success rate over {n_runs} runs:")
+    print(f"Significance level alpha {i+1}/{n_alpha} = {alpha}")
     power_a = dict(zip(metrics, [0] * len(metrics)))
 
     for _ in tqdm(range(n_runs)):
         ref_samples = mvn(mean=np.zeros(DIM), cov=np.eye(DIM)).rvs(N_SAMPLES)
-        shift_samples = mvn(mean=np.array([s] * DIM), cov=np.eye(DIM)).rvs(N_SAMPLES)
+        shift_samples = mvn(mean=np.array([mu] * DIM), cov=np.eye(DIM)).rvs(N_SAMPLES)
         null_samples_list = [
             mvn(mean=np.array([0] * DIM), cov=np.eye(DIM)).rvs(N_SAMPLES)
             for _ in range(100)
@@ -66,7 +66,7 @@ plt.legend()
 plt.show()
 
 # # OPIMAL BAYES LDA
-# clf_s = AnalyticGaussianLQDA(dim=DIM, mu=s)
+# clf_mu = AnalyticGaussianLQDA(dim=DIM, mu=mu)
 # power = dict(zip(metrics, [[] for _ in range(len(metrics))]))
 # for i, alpha in enumerate(np.linspace(0, 1, n_alpha)):
 #     print(f"alpha {i+1}/{n_alpha} = {alpha}")
@@ -75,7 +75,7 @@ plt.show()
 
 #     for _ in tqdm(range(n_runs)):
 #         ref_samples = mvn(mean=np.zeros(DIM), cov=np.eye(DIM)).rvs(N_SAMPLES)
-#         shift_samples = mvn(mean=np.array([s] * DIM), cov=np.eye(DIM)).rvs(N_SAMPLES)
+#         shift_samples = mvn(mean=np.array([mu] * DIM), cov=np.eye(DIM)).rvs(N_SAMPLES)
 #         null_samples_list = [
 #             mvn(mean=np.array([0] * DIM), cov=np.eye(DIM)).rvs(N_SAMPLES)
 #             for _ in range(100)
@@ -88,7 +88,7 @@ plt.show()
 #         P=ref_samples,
 #         Q=shift_samples,
 #         null_samples_list=null_samples_list,
-#         clf_data=clf_s,
+#         clf_data=clf_mu,
 #         clf_null=AnalyticGaussianLQDA(dim=DIM),
 #         single_class_eval=True,
 #         # n_runs=300,

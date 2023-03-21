155d32d6d1f0e22faf36feb66bf953a064756491
JuliaLinhart <julia_linhart@yahoo.com>
2023-03-21 22:22:27 +0100

    error in power_eval and doc

--- eval_clf_test_stats.py
+++ eval_clf_test_stats.py
@@ -1,4 +1,5 @@
-""" Empirical power analysis as in [Lee et al. (2018)](https://arxiv.org/abs/1812.08927) ~cf. Fig.2"""
+# Empirical power analysis
+# see Figure 2 in [Lee et al. (2018)](https://arxiv.org/abs/1812.08927)
 
 import numpy as np
 from scipy.stats import multivariate_normal as mvn
@@ -9,83 +10,87 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from functools import partial
 
-N_SAMPLES = 100
-N_TRIALS_NULL = 100
-N_RUNS = 300
-N_ALPHA = 20
+# GLOBALS
+N_SAMPLES = 100  # number of samples from P and Q ('n0 = n1' in [Lee et al. (2018)])
+N_TRIALS_NULL = 100  # number of samples of the test statistic under (H0) ('B' in [Lee et al. (2018)])
+N_RUNS = 300  # number of test runs to compute the empirical power
+N_ALPHA = 20  # number of significance levels alpha in (0,1)
 
-DIM = 5
-mu = np.sqrt(0.05)
-# DIM = 20
-# mu = np.sqrt(0.01)
+DIM = 5  # dimension of the Gaussians
+mu = np.sqrt(0.05)  # mean shift between P and Q
+
+# DIM = 20 # dimension of the Gaussians
+# mu = np.sqrt(0.01) # mean shift between P and Q
 
 
 def empirical_power_c2st_mean_shift(
     eval_c2st_fn, metrics, metrics_cv, n_runs=N_RUNS, n_alpha=N_ALPHA,
 ):
-    """Computes the empirical power of the test as the True Positive Rate (TPR), 
-    i.e. how many times we (correctly) reject the null hypothesis (H0) over several 
-    test runs performed under the alternative hypothesis (H1) and over a range of 
-    possible significance levels alpha in (0,1).
+    """Computes the empirical power of the Classifier Two Sample Test (C2ST) between 
+    (multivariate) Gaussians with different mean P ~ N(0,1) and Q ~ N(mu,1):
 
-    The considered test is a Classifier Two Sample Test (C2ST) between (multivariate) 
-    Gaussians with different mean P ~ N(0,1) and Q ~ N(mu,1):
-
-                        (H0): P=Q vs. (H1): P \neq Q
-
-    The TPR here corresponds to the number of times we reject (H0) for a test statistic 
-    (or metric) that was estimated on data from P and Q (under (H1)).
-
-    -----------
-    inputs:
-    - eval_c2st_fn: function 
-        built on `eval_htest` with additional inputs:
-            * samples from P, Q 
-            * a list of samples from P to compute the test statistic under (H0)
-    - metrics: list of str
-        contains the names of the metrics that are used in the scoring function 
-        of the test (as in vanillaC2ST.c2st_scores) (= test statistics) 
-    - metrics_cv: list of str
-        subset of metrics we want to compute over several cross-val folds
-    - n_runs: int
-        number of test runs to compute TPR for a given significance level alpha
-    - n_alpha: int
-        number of significance levels alpha in (0,1)
-        --> alpha in `np.linspace(0,1, n_alpha)`
+                            (H0): P=Q vs. (H1): P \neq Q 
     
-    -----------
-    returns:
-    - power: dict 
-        contains the TPR of each metric, computed for every alpha   
-            * keys: metrics+metrics_cv
-            * values: list of TPRs computed for each alpha
+    over a range of significance levels alpha in (0,1).
     
+    The empirical power of a test is defined as the True Positive Rate (TPR), 
+    i.e. how many times we (correctly) reject the null hypothesis (H0) over several test 
+    runs performed under (H1). Here this corresponds to a test statistic that 
+    was estimated on data from P and Q (different from P).
+
+    Args:
+        eval_c2st_fn (function): built on `eval_htest` with additional inputs:
+            - samples from P, Q 
+            - a list of samples from P to compute the test statistic under (H0)
+        metrics (list of str): contains the names of the metrics that are used in the scoring function 
+            of the test (as in vanillaC2ST.c2st_scores) (= test statistics) 
+        metrics_cv (list of str): subset of metrics we want to compute over several cross-val folds
+        n_runs (int, optional): number of test runs to compute TPR for a given significance level alpha. 
+            Defaults to N_RUNS.
+        n_alpha (int, optional): number of significance levels alpha in (0,1). 
+            --> alpha in `np.linspace(0,1, n_alpha)`
+            Defaults to N_ALPHA.
+
+    Returns:
+        emp_power (dict): contains the TPR of each metric, computed for every alpha   
+            - keys: metrics + metrics_cv
+            - values: list of TPRs computed for each alpha
     """
+
+    # combine metrics and metrics_cv
     all_metrics = metrics + metrics_cv
-    print(all_metrics)
+
+    # initialize dict with empty lists
     emp_power = dict(zip(all_metrics, [[] for _ in range(len(all_metrics))]))
 
+    # loop over significance levels alpha
     for i, alpha in enumerate(np.linspace(0, 1, n_alpha)):
 
         print(f"Significance level alpha = {alpha} ({i+1}/{n_alpha})")
 
-        power_a = dict(zip(all_metrics, [0] * len(all_metrics)))
+        # if alpha == 0, we don't need to perform the test
         if alpha == 0:
             for m in all_metrics:
                 emp_power[m].append(0)
-        else:
-            for _ in tqdm(range(n_runs), desc="Empirical error / success rate"):
 
+        else:
+            # initialize dict with 0s
+            emp_power_a = dict(zip(all_metrics, [0] * len(all_metrics)))
+
+            # loop over test runs
+            for _ in tqdm(range(n_runs), desc="Empirical error / success rate"):
+                # generate samples from P and Q
                 ref_samples = mvn(mean=np.zeros(DIM), cov=np.eye(DIM)).rvs(N_SAMPLES)
                 shift_samples = mvn(mean=np.array([mu] * DIM), cov=np.eye(DIM)).rvs(
                     N_SAMPLES
                 )
+                # generate samples from P to compute the test statistic under (H0)
                 null_samples_list = [
                     mvn(mean=np.array([0] * DIM), cov=np.eye(DIM)).rvs(N_SAMPLES)
                     for _ in range(N_TRIALS_NULL)
                 ]
-
-                success_rate = eval_c2st_fn(
+                # evaluate test under (H1)
+                reject_test = eval_c2st_fn(
                     metrics=metrics,
                     conf_alpha=alpha,
                     P=ref_samples,
@@ -93,10 +98,12 @@ def empirical_power_c2st_mean_shift(
                     null_samples_list=null_samples_list,
                     cross_val=False,
                 )
+                # update the empirical power at alpha for each metric
                 for m in metrics:
-                    power_a[m] += success_rate[m] / n_runs
+                    emp_power_a[m] += reject_test[m] / n_runs
 
-                success_rate_cv = eval_c2st_fn(
+                # evaluate test under (H1) over several cross-val folds
+                reject_test_cv = eval_c2st_fn(
                     metrics=metrics_cv,
                     conf_alpha=alpha,
                     P=ref_samples,
@@ -105,44 +112,56 @@ def empirical_power_c2st_mean_shift(
                     cross_val=True,
                     n_folds=2,
                 )
-
+                # update the empirical power at alpha for each cv-metric
                 for m in metrics_cv:
-                    power_a[m] += success_rate_cv[m] / n_runs
+                    emp_power_a[m] += reject_test_cv[m] / n_runs
 
-            print("Result: ", power_a)
+            print("Result: ", emp_power_a)
             print()
 
+            # append the empirical power at alpha for each metric
             for m in all_metrics:
-                emp_power[m].append(power_a[m])
+                emp_power[m].append(emp_power_a[m])
 
-    return power
+    return emp_power
 
 
 if __name__ == "__main__":
     from valdiags.vanillaC2ST import t_stats_c2st
     from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
+    # metrics / test statistics
     metrics = ["accuracy", "div", "mse"]
     metrics_cv = ["accuracy_cv"]
 
+    # whether to use single class evaluation
+    single_class_eval = (
+        True  # if True, we evaluate the classifier on samples from P only
+    )
+
     # ESTIMATED LDA
+
+    # define function to evaluate the test
     eval_c2st_lda = partial(
         eval_htest,
         t_stats_estimator=t_stats_c2st,
         clf_class=LinearDiscriminantAnalysis,
         clf_kwargs={},
-        single_class_eval=True,
+        single_class_eval=single_class_eval,
         verbose=False,
     )
 
-    power = empirical_power_c2st_mean_shift(
+    # compute empirical power for each metric with `eval_c2st_lda``
+    emp_power = empirical_power_c2st_mean_shift(
         eval_htest_fn=eval_c2st_lda, metrics=metrics, metrics_cv=metrics_cv
     )
 
+    # plot empirical power for each metric
     for m in metrics + metrics_cv:
-        plt.plot(np.linspace(0, 1, N_ALPHA), power[m], label=str(m), marker="o")
+        plt.plot(np.linspace(0, 1, N_ALPHA), emp_power[m], label=str(m), marker="o")
     plt.legend()
-    plt.savefig(f"emp_power_lqda_single_class_dim_{DIM}.pdf")
+    # save plot
+    plt.savefig(f"emp_power_lqda_dim_{DIM}_single_class{single_class_eval}.pdf")
     plt.show()
 
     # # OPIMAL BAYES LDA
--- valdiags/test_utils.py
+++ valdiags/test_utils.py
@@ -1,57 +1,50 @@
+# Utils for hypothesis testing:
+# - compute p-value
+# - evaluate hypothesis test
+
 import numpy as np
 
 
 def compute_pvalue(t_stat_est, t_stats_null):
-    """ Computes the p-value of a hypothesis test as the empirical estimate of:
+    """Computes the p-value of a hypothesis test as the empirical estimate of:
                     
-                        p = Prob(T > \hat{T} | H0) 
+        p = Prob(T > \hat{T} | H0) 
         
         which represents the probability of making a type 1 error, i.e. the probability 
         of falsly rejecting the null hypothesis (H0).
-    
-    -------
-    inputs:
-    - t_stat_est: float
-        test statistic \hat{T} estimated on observed data
-    - t_stats_null: list or array 
-        a sample of the test statistic drawn under the null hypothesis: 
-            --> t_i ~ T|(H0)
-    
-    -------
-    returns:
-    - p_value: float
+
+    Args:
+        t_stat_est (float): test statistic \hat{T} estimated on observed data.
+        t_stats_null (list or array): a sample {ti} of the test statistic drawn under (H0): 
+            --> t_i ~ T|(H0).
+
+    Returns:
+        float: empirical p-value: 1/n * \sum_{i=1}^n 1_{t_i > \hat{T}}, ti ~ T|(H0).
     """
     return (t_stat_est < np.array(t_stats_null)).mean()
 
 
 def eval_htest(t_stats_estimator, metrics, conf_alpha=0.05, **kwargs):
-    """ Computes the result of a hypothesis test at a given significance level.
-    A test is rejected if the p-value is lower than the given significance level,
-    meaning that the probability of making a type 1 error will be "small enough". 
+    """ Evaluates a hypothesis test at a given significance level.
 
-    -------
-    inputs:
-    - t_stats_estimator: function that
-        * takes as input a list of metrics that computed on an observed data sample will 
-        give an estimate of the corresponding test statistic
-        * returns objects taken as inputs in `compute_pvalue` 
-        (i.e. test statistic estimated on observed data and drawn under the null hypothesis)
-    - metrics: list of str
-        contains the names of the metrics used in `t_stats_estimator`
-    - conf_alpha: float
-        significance level of the test, yielding a confidence level of (1-alpha).
-    - kwargs: dict
-        --> any additional kwarg of the function `t_stats_estimator` can be added
+    Args:
+        t_stats_estimator (function):  
+            - takes as input a list of metrics that will give an estimate
+            of the corresponding test statistics when computed on an observed data sample.
+            - returns objects taken as inputs in `compute_pvalue` (i.e. test statistic
+            estimated on observed data and drawn under the null hypothesis)
+        metrics (list of str): contains the names of the metrics used in `t_stats_estimator`
+        conf_alpha (float, optional): significance level of the test, yielding a confidence level
+            of 1 - conf_alpha. Defaults to 0.05.
+        kwargs: additional inputs to `t_stats_estimator`: True rejected, False otherwise.
 
-    -------
-    returns:
-    - reject: bool
-        True if the test is rejected, False otherwise. 
+    Returns:
+        dict: contains the result of the hypothesis test for each metric
     """
     reject = {}
     t_stat_data, t_stats_null = t_stats_estimator(metrics=metrics, **kwargs)
     for m in metrics:
         p_value = compute_pvalue(t_stat_data[m], t_stats_null[m])
         reject[m] = p_value < conf_alpha  # True = reject
-    return reject
 
+    return reject
